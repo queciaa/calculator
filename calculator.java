63fd9fe1a712541d9e6b42ecc4017cb72c92759e
queciaa <114770554+queciaa@users.noreply.github.com>
2023-02-24 14:45:05 +0300

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class calculator {
+public class Calculator {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter the first number: ");
